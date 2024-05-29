76ec81a306ec11f243eab660850ab11791126cd0
Brian <bstanton773@gmail.com>
2024-05-29 14:05:30 -0500

    Client side: Enter username functionality

--- static/js/main.js
+++ static/js/main.js
@@ -3,17 +3,49 @@ console.log('Hello from main.js!')
 
 const socket = io()
 
+///////////////////////////////////////////////////////
+// Testing that the socket can send and receive data //
+///////////////////////////////////////////////////////
 
-socket.send('Hello from the client side')
+// socket.send('Hello from the client side')
 
-socket.send({"name": "Brian"})
+// socket.send({"name": "Brian"})
 
-socket.on('message', (data) => {
-    console.log("The client handle message received:", data)
-})
+// socket.on('message', (data) => {
+//     console.log("The client handle message received:", data)
+// })
 
-socket.emit('custom_test_event', {"test": 123})
+// socket.emit('custom_test_event', {"test": 123})
 
-socket.on('another_event', (data) => {
-    console.log('Another event received:', data)
+// socket.on('another_event', (data) => {
+//     console.log('Another event received:', data)
+// })
+
+// Create a loggedInUser
+
+let loggedInUser = null;
+
+const usernameForm = document.getElementById('username-form');
+
+// Add submit event listener to username form to set the loggedInUser
+usernameForm.addEventListener('submit', (e) => {
+    e.preventDefault()
+    // Get the username from the input value
+    let newUser = e.target.username.value
+    // If the input is blank throw a warning in the console
+    if (!newUser){
+        console.warn('Username cannot be blank')
+        return
+    }
+    // Set the global(ish) loggedInUser to the newUser
+    loggedInUser = newUser
+    // Add the username to the Welcome h1
+    welcome = document.getElementById('welcome-banner')
+    welcome.innerText = 'Welcome to the Coding Temple Chat, ' + loggedInUser +  "!"
+    // Hide the username row
+    usernameRow = document.getElementById('usernameRow')
+    usernameRow.style.display = 'none';
+    // Show the messages row
+    messageRow = document.getElementById('messageRow')
+    messageRow.style.display = 'block'
 })
